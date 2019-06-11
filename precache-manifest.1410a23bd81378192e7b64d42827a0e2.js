149629d6e4da203de2d1a35b0a682db6caab116b
Anjana Hengaju <an.hengaju@gmail.com>
2019-06-10 19:48:28 -0500

    Updates

---
+++ precache-manifest.1410a23bd81378192e7b64d42827a0e2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e1d284df1242938d1a15deb54c8a0054",
+    "url": "/clicky-game/./index.html"
+  },
+  {
+    "revision": "56e1fa67f3f7267ea105",
+    "url": "/clicky-game/./static/css/main.583ddec4.chunk.css"
+  },
+  {
+    "revision": "874525917f8d9cf8dd89",
+    "url": "/clicky-game/./static/js/2.90fe8d45.chunk.js"
+  },
+  {
+    "revision": "56e1fa67f3f7267ea105",
+    "url": "/clicky-game/./static/js/main.5967149b.chunk.js"
+  },
+  {
+    "revision": "ab48445bc794ab3cc9b0",
+    "url": "/clicky-game/./static/js/runtime~main.dfe4c287.js"
+  }
+]);
